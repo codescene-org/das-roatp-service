361788b273bb595b92dcf3018841769940ac9242
Dave Read <david.read@digital.education.gov.uk>
2019-03-05 09:26:13 +0000

    APR-159 fix date time formatting on download register

--- src/SFA.DAS.RoATPService.Database/StoredProcedures/RoATP_Audit_History.sql
+++ src/SFA.DAS.RoATPService.Database/StoredProcedures/RoATP_Audit_History.sql
@@ -7,15 +7,15 @@ AS
 	  ,[PreviousValue] AS [Old value]
 	  ,CASE WHEN [newPreviousStatusDate] = '0' THEN CreatedAT ELSE [newPreviousStatusDate] END AS [Old status date]
 	  ,[NewValue] AS [New value]
-      ,[UpdatedAt] AS [Change date time]
+      ,FORMAT([UpdatedAt], 'dd/MM/yyyy HH:mm:ss') AS [Change date time]
       ,[UpdatedBy] AS [Who]
 	FROM (
 	SELECT 
 		au1.*, og1.LegalName, og1.UKPRN
-		,LAG(Convert(nvarchar,au1.UpdatedAt), 1,0) OVER (PARTITION BY au1.Organisationid ORDER BY au1.UpdatedAt ) AS newPreviousStatusDate,
-		Convert(nvarchar,og1.createdAt) createdAt
+		,LAG(Convert(nvarchar, FORMAT(au1.UpdatedAt, 'dd/MM/yyyy HH:mm:ss')), 1,0) OVER (PARTITION BY au1.Organisationid ORDER BY au1.UpdatedAt ) AS newPreviousStatusDate,
+		Convert(nvarchar, FORMAT(og1.createdAt, 'dd/MM/yyyy HH:mm:ss')) createdAt
 		FROM [Audit] au1
 		LEFT JOIN Organisations og1 ON og1.Id = au1.OrganisationId
 	) ab1
-	ORDER BY LegalName, ab1.Updatedat desc 
+	ORDER BY LegalName, ab1.Updatedat desc
 GO
