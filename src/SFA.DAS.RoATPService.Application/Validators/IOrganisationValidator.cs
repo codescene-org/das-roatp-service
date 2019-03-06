dd9998955609a19af2c1275af395df4f014a7ab6
Dave Read <david.read@digital.education.gov.uk>
2019-03-06 15:41:27 +0000

    APR-233 API validation for create organisation endpoint

--- src/SFA.DAS.RoATPService.Application/Validators/IOrganisationValidator.cs
+++ src/SFA.DAS.RoATPService.Application/Validators/IOrganisationValidator.cs
@@ -1,14 +1,19 @@
 ﻿namespace SFA.DAS.RoATPService.Application.Validators
 {
+    using SFA.DAS.RoATPService.Domain;
     using System;
 
     public interface IOrganisationValidator
     {
         bool IsValidOrganisationId(Guid organisationId);
-        bool IsValidProviderTypeId(int providerTypeId);
+        bool IsValidProviderType(ProviderType providerType);
         bool IsValidUKPRN(long ukPrn);
         bool IsValidLegalName(string legalName);
+        bool IsValidTradingName(string tradingName);
         bool IsValidStatusDate(DateTime statusDate);
-        bool IsValidStatus(int status);
+        bool IsValidStatus(OrganisationStatus status);
+        bool IsValidCompanyNumber(string companyNumber);
+        bool IsValidCharityNumber(string charityNumber);
+        bool IsValidOrganisationType(OrganisationType organisationType);
     }
 }
