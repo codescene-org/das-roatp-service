400e8ebce951dcc4972a2982cafd6075aed73ac2
MarkFCain <mark.cain@digital.education.gov.uk>
2019-03-12 10:47:38 +0000

    APR-328 xlsx download added

--- src/SFA.DAS.RoATPService.Database/StoredProcedures/RoATP_CSV_SUMMARY.sql
+++ src/SFA.DAS.RoATPService.Database/StoredProcedures/RoATP_CSV_SUMMARY.sql
@@ -3,7 +3,7 @@ AS
 
 SET NOCOUNT ON
 
- select ukprn AS UKPRN, 
+  select ukprn AS UKPRN, 
  LegalName + 
 	 case isnull(tradingName,'') when '' then ''
 	 else ' T/A ' + 
@@ -17,8 +17,9 @@ SET NOCOUNT ON
 	WHEN 'true' then 'Y' else 'N' end AS 'Parent company guarantee',
  case Json_value(OrganisationData,'$.FinancialTrackRecord')
 	WHEN 'true' then 'N' else 'Y' end AS 'New Organisation without financial track record',
- Json_value(OrganisationData,'$.StartDate') AS 'Start Date',
- Case StatusId WHEN 0 then StatusDate else null END AS 'End Date',
- CASE StatusId WHEN 2 THEN StatusDate ELSE NULL END AS 'Provider not currently starting new apprentices'
+ convert(varchar(10),convert(datetime,Json_value(OrganisationData,'$.StartDate')), 103) AS  'Start Date',
+ Case StatusId WHEN 0 then convert(varchar(10),StatusDate,103) else null END AS 'End Date',
+ CASE StatusId WHEN 2 THEN convert(varchar(10),StatusDate,103) ELSE NULL END AS 'Provider not currently starting new apprentices'
  from organisations o 
  left outer join providerTypes pt on o.ProviderTypeId = pt.Id
+ order by LegalName
