a56f6c17e56037a660ecea1c3b04318f812bbea3
MarkFCain <mark.cain@digital.education.gov.uk>
2019-03-12 16:17:02 +0000

    APR-330 added code for story and started scaffolding for integration tests

---
+++ src/SFA.DAS.RoatpService.Data.IntegrationTests/Handlers/OrganisationHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFA.DAS.RoatpService.Data.IntegrationTests.Models;
+using SFA.DAS.RoatpService.Data.IntegrationTests.Services;
+using SFA.DAS.RoATPService.Domain;
+
+namespace SFA.DAS.RoatpService.Data.IntegrationTests.Handlers
+{
+    public static class OrganisationHandler
+    {
+        private static readonly DatabaseService DatabaseService = new DatabaseService();
+
+        public static void InsertRecord(OrganisationModel organisation)
+        {
+            var sql =
+                @"INSERT INTO [Organisations] ([Id] ,[CreatedAt],[CreatedBy],[UpdatedAt],[UpdatedBy],[StatusId],[ProviderTypeId],[OrganisationTypeId],
+                    [UKPRN],[LegalName],[TradingName],[StatusDate],[OrganisationData]) VALUES " +
+                $@"(@id,@createdAt, @createdBy, @updatedAt, @updatedBy, @statusId, @providerTypeId, @organisationTypeId, 
+                    @ukprn, @legalName, @tradingName,@statusDate,  @organisationData); ";
+
+            DatabaseService.Execute(sql, organisation);
+        }
+
+        public static void InsertRecords(List<OrganisationModel> organisations)
+        {
+            foreach (var org in organisations)
+            {
+                InsertRecord(org);
+            }
+        }
+
+        public static OrganisationModel GetOrganisationFromId(Guid organisationId)
+        {
+            var organisationModel = DatabaseService.Get<OrganisationModel>($@"select * from Organisations where Id = '{organisationId}'");
+            return organisationModel;
+        }
+
+        public static void DeleteRecord(Guid id)
+        {
+            var idToDelete = SqlStringService.ConvertStringToSqlValueString(id.ToString());
+            var sql = $@"DELETE from Organisations where id = {idToDelete}";
+            DatabaseService.Execute(sql);
+        }
+
+        public static void DeleteRecords(List<Guid> ids)
+        {
+            foreach (var id in ids)
+            {
+                DeleteRecord(id);
+            }
+        }
+
+        public static void DeleteAllRecords()
+        {
+            var sql = $@"DELETE from Organisations";
+            DatabaseService.Execute(sql);
+        }
+    }
+}
---
+++ src/SFA.DAS.RoatpService.Data.IntegrationTests/Services/DatabaseService.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using SFA.DAS.RoatpService.Data.IntegrationTests.Models;
+
+namespace SFA.DAS.RoatpService.Data.IntegrationTests.Services
+{
+    public class DatabaseService
+    {
+        public DatabaseService()
+        {
+
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("connectionStrings.Local.json")
+                .Build();
+            WebConfiguration = new TestWebConfiguration
+            {
+                SqlConnectionString = Configuration.GetConnectionString("SqlConnectionStringTest")
+            };
+        }
+
+        //public AssessorDbContext TestContext
+        //{
+        //    get
+        //    {
+        //        var sqlConnectionStringTest = Configuration.GetConnectionString("SqlConnectionStringTest");
+        //        var option = new DbContextOptionsBuilder<AssessorDbContext>();   <=== this will be wrong
+        //        option.UseSqlServer(sqlConnectionStringTest, options => options.EnableRetryOnFailure(3));
+        //        return new AssessorDbContext(option.Options);
+        //    }
+        //}
+
+
+        public IConfiguration Configuration { get; }
+        public TestWebConfiguration WebConfiguration;
+
+        public void SetupDatabase()
+        {
+            DropDatabase();
+
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("SqlConnectionString")))
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+
+                var comm = new SqlCommand
+                {
+                    Connection = connection,
+                    CommandText =
+                        $@"DBCC CLONEDATABASE ('SFA.DAS.RoatpService.Database', 'SFA.DAS.RoatpService.Database.Test'); ALTER DATABASE [SFA.DAS.RoatpService.Database.Test] SET READ_WRITE;"
+                };
+                var reader = comm.ExecuteReader();
+                reader.Close();
+            }
+        }
+
+        public void Execute(string sql)
+        {
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("SqlConnectionStringTest")))
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                connection.Execute(sql);
+                connection.Close();
+            }
+        }
+
+        public T Get<T>(string sql)
+        {
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("SqlConnectionStringTest")))
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                var result = connection.Query<T>(sql);
+                connection.Close();
+                return result.FirstOrDefault();
+            }
+        }
+
+
+        public object ExecuteScalar(string sql)
+        {
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("SqlConnectionStringTest")))
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                var result = connection.ExecuteScalar(sql);
+                connection.Close();
+
+                return result;
+            }
+        }
+
+
+        public void Execute(string sql, TestModel model)
+        {
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("SqlConnectionStringTest")))
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+                connection.Execute(sql, model);
+                connection.Close();
+            }
+        }
+
+        public void DropDatabase()
+        {
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("SqlConnectionString")))
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+
+                var comm = new SqlCommand
+                {
+                    Connection = connection,
+                    CommandText =
+                        $@"if exists(select * from sys.databases where name = 'SFA.DAS.RoatpService.Database.Test') BEGIN ALTER DATABASE [SFA.DAS.RoatpService.Database.Test] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;  DROP DATABASE [SFA.DAS.RoatpService.Database.Test]; END"
+                };
+                var reader = comm.ExecuteReader();
+                reader.Close();
+            }
+        }
+    }
+}
