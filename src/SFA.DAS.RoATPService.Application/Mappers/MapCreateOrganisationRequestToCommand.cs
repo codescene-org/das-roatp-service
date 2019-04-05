0b6d12362f4742ae51d914d076391f8be9119269
MarkFCain <mark.cain@digital.education.gov.uk>
2019-04-05 08:59:39 +0100

    APR-388 added code

--- src/SFA.DAS.RoATPService.Application/Mappers/MapCreateOrganisationRequestToCommand.cs
+++ src/SFA.DAS.RoATPService.Application/Mappers/MapCreateOrganisationRequestToCommand.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using SFA.DAS.RoATPService.Api.Types.Models;
 using SFA.DAS.RoATPService.Application.Commands;
 using SFA.DAS.RoATPService.Application.Interfaces;
@@ -9,26 +7,26 @@ namespace SFA.DAS.RoATPService.Application.Mappers
 {
     public class MapCreateOrganisationRequestToCommand: IMapCreateOrganisationRequestToCommand
     {
+        private const int MainProviderTypeId = 1;
+        private const int EmployerProviderTypeId = 2;
+        private const int SupportingProviderTypeId = 3;
+
+        private const int OrganisationStatusActive = 1;
+        private const int OrganisationStatusOnboarding = 3;
+
         public CreateOrganisationCommand Map(CreateOrganisationRequest request)
-        {
-            const int mainProviderTypeId = 1;
-            const int employerProviderTypeId = 2;
-            const int supportingProviderTypeId = 3;
-
-            const int organisationStatusOnboarding = 3;
-            const int organisationStatusActive = 1;
-
+        {     
             int organisationStatusId;
             DateTime? startDate = null;
 
             switch (request.ProviderTypeId)
             {
-                case mainProviderTypeId:
-                case employerProviderTypeId:
-                    organisationStatusId = organisationStatusOnboarding;
+                case MainProviderTypeId:
+                case EmployerProviderTypeId:
+                    organisationStatusId = OrganisationStatusOnboarding;
                     break;
-                case supportingProviderTypeId:
-                    organisationStatusId = organisationStatusActive;
+                case SupportingProviderTypeId:
+                    organisationStatusId = OrganisationStatusActive;
                     startDate = DateTime.Today;
                     break;
 
