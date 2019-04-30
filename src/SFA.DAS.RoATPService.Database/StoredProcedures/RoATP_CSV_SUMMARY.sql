8ebc4fd1ed971faf36d63130bee8f8882aca06fa
Dave Read <david.read@digital.education.gov.uk>
2019-04-30 13:37:02 +0100

    APR-389 altered date format so that dates can be parsed by indexer

--- src/SFA.DAS.RoATPService.Database/StoredProcedures/RoATP_CSV_SUMMARY.sql
+++ src/SFA.DAS.RoATPService.Database/StoredProcedures/RoATP_CSV_SUMMARY.sql
@@ -15,9 +15,9 @@ select ukprn AS UKPRN,
 	WHEN 'true' then 'Y' else 'N' end AS 'Parent company guarantee',
  case Json_value(OrganisationData,'$.FinancialTrackRecord')
 	WHEN 'true' then 'N' else 'Y' end AS 'New Organisation without financial track record',
- convert(varchar(10),convert(date,Json_value(OrganisationData,'$.StartDate')), 103) AS  'Start Date',
- Case StatusId WHEN 0 then convert(varchar(10),StatusDate,103) else null END AS 'End Date',
- CASE StatusId WHEN 2 THEN convert(varchar(10),StatusDate,103) ELSE NULL END AS 'Provider not currently starting new apprentices'
+ convert(varchar(10),convert(date,Json_value(OrganisationData,'$.StartDate')), 111) AS  'Start Date',
+ Case StatusId WHEN 0 then convert(varchar(10),StatusDate,111) else NULL END AS 'End Date',
+ CASE StatusId WHEN 2 THEN convert(varchar(10),StatusDate,111) ELSE NULL END AS 'Provider not currently starting new apprentices'
  from organisations o 
  left outer join providerTypes pt on o.ProviderTypeId = pt.Id
  where o.StatusId in (0,1,2) -- exclude on-boarding
