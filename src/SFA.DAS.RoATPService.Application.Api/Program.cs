4e04a0539d3b80641793f57c37451291225b245a
Dave Read <david.read@digital.education.gov.uk>
2019-07-22 09:11:40 +0100

    APR-581 revert in process hosting, use Kestrel instead of IIS This is due to performance overheads introduced by using IIS and the in process hosting does not fix environment stablility issues

--- src/SFA.DAS.RoATPService.Application.Api/Program.cs
+++ src/SFA.DAS.RoATPService.Application.Api/Program.cs
@@ -31,7 +31,7 @@
             return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
-                .UseIIS()
+                .UseKestrel()
                 .UseNLog();
         }
     }
