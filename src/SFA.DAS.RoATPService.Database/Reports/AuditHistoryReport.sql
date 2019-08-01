92277920a973e772b8bef4766b3b3a46a5393437
Dave Read <david.read@digital.education.gov.uk>
2019-08-01 12:49:56 +0100

    APR-620 remove unused scripts from initial setup

--- src/SFA.DAS.RoATPService.Database/Reports/AuditHistoryReport.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[AuditHistoryReport]
-AS
-	SELECT o.UKPRN, ah.UpdatedAt, ah.UpdatedBy, ah.FieldChanged, ah.PreviousValue, ah.NewValue FROM Audit ah
-	INNER JOIN Organisations o 
-	ON o.Id = ah.OrganisationId
-
-RETURN 0
