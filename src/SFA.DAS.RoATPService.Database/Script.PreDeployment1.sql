c0b12480a9c3ad6faf45c97c057b46e0c771d03f
Dave Read <david.read@digital.education.gov.uk>
2019-08-13 19:55:53 +0100

    Removed unused scripts from pre and post deploy processes

--- src/SFA.DAS.RoATPService.Database/Script.PreDeployment1.sql
+++ src/SFA.DAS.RoATPService.Database/Script.PreDeployment1.sql
@@ -9,27 +9,3 @@
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-
-IF EXISTS (
-	SELECT 1 from INFORMATION_SCHEMA.COLUMNS
-	WHERE TABLE_NAME = 'Audit'
-	AND COLUMN_NAME = 'FieldChanged'
-	)
-BEGIN
-	DELETE FROM dbo.[Audit]
-
-END
-
-IF EXISTS (
-    SELECT 1 from INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_NAME = 'Organisations'
-)
-BEGIN
-	IF EXISTS (
-		SELECT 1 FROM dbo.Organisations WHERE OrganisationTypeId NOT IN (0)
-		)
-	BEGIN
-		UPDATE dbo.Organisations
-		SET OrganisationTypeId = 0
-	END
-END
