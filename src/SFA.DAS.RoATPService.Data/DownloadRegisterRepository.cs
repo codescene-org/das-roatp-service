d9a60b05e20ffc631935a765b47db67d80e5c4bb
Dave Read <david.read@digital.education.gov.uk>
2019-08-21 13:31:35 +0100

    APR-662 ensure that connections are closed in download repository

--- src/SFA.DAS.RoATPService.Data/DownloadRegisterRepository.cs
+++ src/SFA.DAS.RoATPService.Data/DownloadRegisterRepository.cs
@@ -17,44 +17,81 @@ namespace SFA.DAS.RoATPService.Data
         private const string CompleteRegisterStoredProcedure = "[dbo].[RoATP_Complete_Register]";
         private const string AuditHistoryStoredProcedure = "[dbo].[RoATP_Audit_History]";
         private const string RoatpCsvSummary = "[dbo].[RoATP_CSV_SUMMARY]";
-
-        private readonly SqlConnection _connection;
-
+        
         public DownloadRegisterRepository(IWebConfiguration configuration)
         {
             _configuration = configuration;
-            _connection = new SqlConnection(_configuration.SqlConnectionString);
-            if (_connection.State != ConnectionState.Open)
-            {
-                _connection.Open();
-            }
         }
 
         public async Task<IEnumerable<IDictionary<string, object>>> GetCompleteRegister()
         {
-            return (await _connection.QueryAsync(CompleteRegisterStoredProcedure, commandType: CommandType.StoredProcedure)).OfType<IDictionary<string, object>>().ToList();
+            using (var connection = new SqlConnection(_configuration.SqlConnectionString))
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                return (await connection.QueryAsync(CompleteRegisterStoredProcedure, 
+                    commandType: CommandType.StoredProcedure)).OfType<IDictionary<string, object>>().ToList();
+            }
         }
 
         public async Task<IEnumerable<IDictionary<string, object>>> GetAuditHistory()
         {
-            return (await _connection.QueryAsync(AuditHistoryStoredProcedure, commandType: CommandType.StoredProcedure)).OfType<IDictionary<string, object>>().ToList();
+            using (var connection = new SqlConnection(_configuration.SqlConnectionString))
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                return (await connection.QueryAsync(AuditHistoryStoredProcedure,
+                    commandType: CommandType.StoredProcedure)).OfType<IDictionary<string, object>>().ToList();
+            }
         }
 
         public async Task<IEnumerable<IDictionary<string, object>>> GetRoatpSummary()
         {
-            return (await _connection.QueryAsync(RoatpCsvSummary, commandType: CommandType.StoredProcedure)).OfType<IDictionary<string, object>>().ToList();
+            using (var connection = new SqlConnection(_configuration.SqlConnectionString))
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                return (await connection.QueryAsync(RoatpCsvSummary, commandType: CommandType.StoredProcedure))
+                    .OfType<IDictionary<string, object>>().ToList();
+            }
         }
-
-
+        
         public async Task<IEnumerable<IDictionary<string, object>>> GetRoatpSummaryUkprn(int ukprn)
         {
-            return (await _connection.QueryAsync(RoatpCsvSummary, new{ukprn},commandType: CommandType.StoredProcedure)).OfType<IDictionary<string, object>>().ToList();
+            using (var connection = new SqlConnection(_configuration.SqlConnectionString))
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                
+                return (await connection.QueryAsync(RoatpCsvSummary, new {ukprn},
+                    commandType: CommandType.StoredProcedure)).OfType<IDictionary<string, object>>().ToList();
+            }
         }
 
         public async Task<DateTime?> GetLatestNonOnboardingOrganisationChangeDate()
         {
-                const string sql = "select max(coalesce(updatedAt,createdAt)) latestDate from organisations o WHERE o.StatusId IN (0,1,2) ";
-                return await _connection.ExecuteScalarAsync<DateTime?>(sql);
+            using (var connection = new SqlConnection(_configuration.SqlConnectionString))
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                const string sql = 
+                    "select max(coalesce(updatedAt,createdAt)) latestDate from organisations o WHERE o.StatusId IN (0,1,2) ";
+                return await connection.ExecuteScalarAsync<DateTime?>(sql);
+            }
         }
     }
 }
